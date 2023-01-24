a7be0a6669a85076d68050aff358dc28d1fd7c8d
amhyou <116261114+amhyou@users.noreply.github.com>
2023-01-24 18:25:51 +0100

    backend deployed in azure

--- services/auth.js
+++ services/auth.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:8000/api/"
+const url = "https://daphner.azurewebsites.net/api/"
 
 export async function makePost(end,data,token=""){
     const req = await fetch(url+end,{
